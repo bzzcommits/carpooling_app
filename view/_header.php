35981c3e2e8b1e0f0a3ffb23f96010cb2ccfa5fe
Ema <ema.dogancic@gmail.com>
2018-05-27 23:03:17 +0000

    Prvi dio uređivanja i dio za dodavanje vožnji.

--- view/_header.php
+++ view/_header.php
@@ -1,16 +1,30 @@
 <?php
-echo '$_SESSION:';  print_r( $_SESSION );    echo "</br></br>";
-echo '$_POST:';  print_r( $_POST );   echo "</br></br>";
-echo '$_GET:';  print_r( $_GET );   echo "</br></br>";
+//echo '$_SESSION:';  print_r( $_SESSION );    echo "</br></br>";
+//echo '$_POST:';  print_r( $_POST );   echo "</br></br>";
+//echo '$_GET:';  print_r( $_GET );   echo "</br></br>";
  ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf8">
-	<title>BlaBlaCar</title>
-	<link rel="stylesheet" href="<?php echo __SITE_URL;?>/css/style.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
-	<script src="<?php echo __SITE_URL;?>/js/skripta.js"></script>
-</head>
-<body>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <!-- Bootstrap core CSS-->
+  <link href="<?php echo __SITE_URL; ?>/view/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="<?php echo __SITE_URL; ?>/view/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Custom styles for this template-->
+  <link href="<?php echo __SITE_URL; ?>/view/css/sb-admin.css" rel="stylesheet">
+
+  <!--
+  <link rel="stylesheet" href="<?php //echo '__SITE_URL';?>/css/style.css">
+  -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+  <script src="<?php echo '__SITE_URL';?>/js/skripta.js"></script>
+
+  <!-- iza ide jos title, po potrebi ako nesto bas za tu stranicu treba i </head> i onda <body> -->
+  
