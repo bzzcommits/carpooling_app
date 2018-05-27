b711b82caa74ed617da7154ecaf70893082de2de
Ema <ema.dogancic@gmail.com>
2018-05-27 22:58:26 +0000

    Prvi dio uređivanja i dio za dodavanje vožnji.

--- controller/pretraziController.php
+++ controller/pretraziController.php
@@ -11,21 +11,59 @@ class PretraziController extends BaseController
 	{
 		$ls = new CarService();
 
-		// Ako nam forma nije u $_POST poslala autora u ispravnom obliku, preusmjeri ponovno na formu.
+		// Ako nam forma nije u $_POST poslala podatke u ispravnom obliku, preusmjeri ponovno na formu.
 		if( !isset( $_POST['start_place'] ) || !preg_match( '/^[a-zA-Z -]+$/', $_POST['start_place'] )
 		 		|| !isset( $_POST['end_place'] ) || !preg_match( '/^[a-zA-Z -]+$/', $_POST['end_place'] )
 				|| !isset( $_POST['date'] ) || !preg_match( '/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $_POST['date'] ))   
 		{
-			header( 'Location: ' . __SITE_URL . '/index.php?rt=pretrazi/index');
+			header( 'Location: ' . __SITE_URL . '/index.php?rt=pretrazi/index' );
 			exit();
 		}
 		//if (isset($_POST['back'])) $this->registry->template->show( 'drive_index' );
 
+		//dodala sam ovo da mogu ispisivati u naslov tablice - Ema
+		$_SESSION['start_place'] = $_POST['start_place'];
+		$_SESSION['end_place'] = $_POST['end_place'];
+		$_SESSION['date'] = $_POST['date'];
+		
 		$this->registry->template->resDrive = $ls->searchDrive( $_POST['start_place'], $_POST['end_place'], $_POST['date'] );
-		$this->registry->template->show( 'drive_search' );
+		$this->registry->template->show( 'drive_index' );
 	}
+	 //to u controller ili u class ???
+	 public function newOffer(){
 
-
+		 // provjeriti, trebalo bi mozda napraviti bolji preg_match, tako da na prvom mj
+		 // mora biti barem 1 slovo, ne moze poceti razmakom ili crticom
+		 //i mozda bi trebalo napraviti da aplikacija nije case sensitive za pretragu i unos,
+		 // npr ja cu unositi sve s malim slovima, pa pretrazivati sve s malim
+		 
+		 //preg match za date uzeti u obzir veljacu i 30 i 31 dan
+		 //preg match za vrijeme uzeti 00, 01, 02, .. 09, 10, 11, 12, .. 19, 20, 21, 22, .. 
+		 if ( !isset( $_POST['start_place_new'] ) || !preg_match( '/^[a-zA-Z -]+$/', $_POST['start_place_new'] )
+			 || !isset( $_POST['end_place_new'] ) || !preg_match( '/^[a-zA-Z -]+$/', $_POST['end_place_new'] )
+			 || !isset( $_POST['date_new'] ) || !preg_match( '/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $_POST['date_new'] )
+			 || !isset( $_POST['start_time_new'] ) || !preg_match( '/^[0-1][0-9]|{2}[0-4]{:}[0-5][0-9]$/', $_POST['start_time_new'] )
+			 || !isset( $_POST['end_time_new'] ) || !preg_match( '/^[0-1][0-9]|{2}[0-4]{:}[0-5][0-9]$/', $_POST['end_time_new'] )
+			 || !isset( $_POST['price_new'] ) || !preg_match( '/%d+/', $_POST['price_new'] )
+			 || !isset( $_POST['seats_new'] ) || !preg_match( '/%d+/', $_POST['seats_new'] ) )
+		{
+			//$message = "Unos nije bio dobar, molimo pokušajte ponovno. ";
+			//provjeriti je li message globalna, kako joj se pristupa
+			// i provjeriti hoce li trebati promijeniti header, kad se izmijeni stranica Drive
+			header( 'Location: ' . __SITE_URL . 'index.php?rt=pretrazi/index' );
+			exit();
+		}
+		
+		$drive = new Drive( $_SESSION['user_id'], $_POST['start_place_new'], 
+							$_POST['end_place_new'], $_POST['date'], $_POST['start_time_new'],
+							$_POST['end_time_new'], $_POST['price_new'],
+							$_POST['seats_new'], $_SESSION['username'] );
+							
+		$car = new CarService();
+		
+		$this->registry->template->newDrive = $car->offerDrive( $drive );
+		$this->registry->template->show( 'drive_index' );
+	 }
 };
 
 ?>
