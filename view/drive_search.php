c0e448c7d3ccec7bdee62d623fa4cece2714b232
Ema <ema.dogancic@gmail.com>
2018-05-27 23:06:37 +0000

    Prvi dio uređivanja i dio za dodavanje vožnji.

--- view/drive_search.php
+++ view/drive_search.php
@@ -1,37 +1,64 @@
-<?php require_once __SITE_PATH . '/view/_header.php';
-require_once __SITE_PATH . '/view/upperRightCorner.php';
-require_once __SITE_PATH . '/view/menu.php';
-
-if ($resDrive == null)
+<?php 
+if ( !isset($resDrive) )
+	return;
+elseif ($resDrive == null)
 {
-  echo "<p>Nema raspolo≈æivih vo≈ænji.</p>";
+  echo "<p>Nema raspoloûivih voûnji.</p>";
 }
 else {
     ?>
-    <table>
-    	<tr><th>Username</th><th>Start place</th><th>End place</th><th>Price</th><th>Number of available seats</th></tr>
-    	<?php
-    foreach( $resDrive as $x )
-		 {
-			    echo '<tr>' .
-			     '<td>' . $x->username . '</td>' .
-           '<td>' . $x->start_time . '</td>' .
-           '<td>' . $x->end_time . '</td>' .
-           '<td>' . $x->price . '</td>' .
-			     '<td>' . $x->place_number. '</td>' .
-			     '</tr>';
-		 }
-  }
+	<div class="content-wrapper">
+	<div class="container-fluid">
+	<div class="card mb-3">
+        <div class="card-header">
+          <i class="fa fa-table"></i> 
+		  <?php echo $_SESSION['start_place'] . " - " . $_SESSION['end_place'] . ". "; ?>
+		  All drives on day <?php echo $_SESSION['date'] . ". ";?>
+		</div>
+		
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+              <thead>
+                <tr>
+                  <th>Userame</th>
+                  <th>Start time</th>
+                  <th>ETA</th>
+                  <th>Price</th>
+                  <th>Number of available seats</th>
+                </tr>
+              </thead>
+              <tfoot>
+                <tr>
+                  <th>Userame</th>
+                  <th>Start time</th>
+                  <th>ETA</th>
+                  <th>Price</th>
+                  <th>Number of available seats</th>
 
-	?>
-</table>
-
-<!--
-
-<button type="submit" name="back" id="back">Back!</button>
-
--->
+                </tr>
+              </tfoot>
+              <tbody>
+			  
+				<?php
+					foreach( $resDrive as $x ){
+						echo '<tr>' .
+							 '<td>' . $x->username . '</td>' .
+						     '<td>' . $x->start_time . '</td>' .
+						     '<td>' . $x->end_time . '</td>' .
+						     '<td>' . $x->price . '</td>' .
+						     '<td>' . $x->place_number. '</td>' .
+							 '</tr>';
+				    }
+				?>
+                
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
 <?php
-require_once __SITE_PATH . '/view/message.php';
-require_once __SITE_PATH . '/view/_footer.php';
+}
 ?>
