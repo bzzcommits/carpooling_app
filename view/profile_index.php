ed1a4baebf05384b80d8918c05c32a0d6ec397e9
Anastasija <anastasija.jezernik@gmail.com>
2018-07-06 23:39:37 +0000

    promijenjen profile_index.php

--- view/profile_index.php
+++ view/profile_index.php
@@ -6,17 +6,18 @@ require_once __SITE_PATH . '/view/_header.php';
         + vrsta i model auta, ukupna ocjena
         + posebno ocjene + komentari
         + nadolazece voznje koje je objavio
-                - omogucen je gumb za otkazivanje voznje (TO JOS TREBA)
+                - omogucen je gumb za otkazivanje voznje
    Za ostale se ispisuju:
-        - nadolazece voznje
-                - omogucen je gumb za otkaz rezervacije (TO JOS TREBA)
+        + nadolazece voznje
+                - omogucen je gumb za otkaz rezervacije
         - voznje koje jos nisu ocijenili
                 - treba dodati formu u koju upise ocjenu, komentar i gumb (TO JOS TREBA)
-        - poruke koje javljaju o otkazivanju rezervirane voznje
-                - dodati gumb koji se klikne nakon sto korisnik procita (TO JOS TREBA)*/
+        + poruke koje javljaju o otkazivanju rezervirane voznje
+                - dodati gumb koji se klikne nakon sto korisnik procita*/
 ?>
 
  <title> Profile </title>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
 </head>
 
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
@@ -101,6 +102,7 @@ require_once __SITE_PATH . '/view/menu.php';
                   echo "From " . $poljeMojihVoznji[$i][0] . "   to: " . $poljeMojihVoznji[$i][1]. "  on: " . $poljeMojihVoznji[$i][2]
                   . "  at: " . $poljeMojihVoznji[$i][3] . " till: " . $poljeMojihVoznji[$i][4] . "  costs: " . $poljeMojihVoznji[$i][5] .  "   no. of reservations: "
                   . $poljeMojihVoznji[$i][6];
+                  echo ' <button class= "otkaziVoznju" name="' . $poljeMojihVoznji[$i][7] . '" >Delete this ride</button>';
                   echo '</div>';
              }
             echo "</div></div>";
@@ -111,6 +113,8 @@ require_once __SITE_PATH . '/view/menu.php';
 
         $poljeRezerv = $poljeRez[0];
         $poljeBezKom = $poljeRez[1];
+        $poljeIzbris = $poljeRez[2];
+
         // ispisujemo rezervirane voznje
         if (count($poljeRezerv) > 0) { ?>
                 <div class="card card-register mx-auto mt-5">
@@ -118,10 +122,13 @@ require_once __SITE_PATH . '/view/menu.php';
                 <div class="card-body">
 
         <?php   for ($i = 0; $i < count($poljeRezerv); ++$i) {
-                        echo '<div class="form-group">';
-                        echo 'Driver: ' . $poljeRezerv[$i][0] . ", from " . $poljeRezerv[$i][1] . "   to: " . $poljeRezerv[$i][2]
-                        . "  on: " . $poljeRezerv[$i][3] . "  at: " . $poljeRezerv[$i][4] . " costs: " . $poljeRezerv[$i][6];
-                        echo '</div>';
+                    echo '<div class="form-group">';
+                    echo 'Driver: ' . $poljeRezerv[$i][0] . ", from " . $poljeRezerv[$i][1] . "   to: " . $poljeRezerv[$i][2]
+                    . "  on: " . $poljeRezerv[$i][3] . "  at: " . $poljeRezerv[$i][4] . " costs: " . $poljeRezerv[$i][6];
+                    // 7.element polja je drive_id. Pomocu njega i gumbda znamo koju voznju treba otkazati
+                    $imeGumba = "otkaziRezervacijuBr_" . $poljeRezerv[$i][7]; // odvajamo s _ da mozemo explodat
+                    echo ' <button class= "otkaziRezervaciju" name="' . $poljeRezerv[$i][7] . '" >Delete</button>';
+                    echo '</div>';
            }
           echo "</div></div>";
         }
@@ -132,24 +139,117 @@ require_once __SITE_PATH . '/view/menu.php';
                 <div class="card-body">
 
         <?php   for ($i = 0; $i < count($poljeBezKom); ++$i) {
+                    echo '<form method="post" action="<?php echo __SITE_URL; ?>/index.php?rt=user/unesenKomentar">';
                         echo '<div class="form-group">';
                         echo 'Driver: ' . $poljeBezKom[$i][0] . ", from " . $poljeBezKom[$i][1] . "   to: " . $poljeBezKom[$i][2]
                         . "  on: " . $poljeBezKom[$i][3] . "  at: " . $poljeBezKom[$i][4] . " costs: " . $poljeBezKom[$i][6];
-                        echo '</div>';
-           }
-          echo "</div></div>";
-        }
 
+                        echo '<input class="form-control" class="unesiOcjenu" placeholder="Enter grade" type="text" name="'.$poljeBezKom[$i][7] .'" />';
+                        echo '<input class="form-control" class="unesiKomentar" placeholder="Enter comment" type="text" name="'.$poljeBezKom[$i][7] .'" />';
+
+                        echo ' <button class= "ocjenjenaVoznja" name="' . $poljeBezKom[$i][7] . '" >Grade this drive!</button>';
+                        echo '</div>';
+                    echo '</form>';
+               }
+              echo "</div></div>";
+
+        }
+        // ispisujemo izbrisane voznje, korisnik treba kliknuti da je procitao poruku
+        if (count($poljeIzbris) > 0) { ?>
+                <div class="card card-register mx-auto mt-5">
+                <div class="card-header">DRIVES THAT ARE CANCELED</div>
+                <div class="card-body">
+
+        <?php   for ($i = 0; $i < count($poljeIzbris); ++$i) {
+                    echo '<div class="form-group">';
+                    echo 'Driver: ' . $poljeIzbris[$i][0] . ", from " . $poljeIzbris[$i][1] . "   to: " . $poljeIzbris[$i][2]
+                    . "  on: " . $poljeIzbris[$i][3] . "  at: " . $poljeIzbris[$i][4] . " costs: " . $poljeIzbris[$i][6];
+                    echo ' <button class= "procitanaPoruka" name="' . $poljeIzbris[$i][7] . '" >Read the message</button>';
+                    echo '</div>';
+               }
+              echo "</div></div>";
+        }
 
     ?>
 
+
+<script>
+$(document).ready( function()
+{
+    $( "body" ).on( "click", "button.otkaziRezervaciju", otkaziRezervaciju );
+    $( "body" ).on( "click", "button.otkaziVoznju", otkaziVoznju );
+    $( "body" ).on( "click", "button.procitanaPoruka", procitanaPoruka );
+});
+function otkaziRezervaciju(event) {
+    var ret = confirm("Are you sure you want to delete this reservation?");
+    if (ret === true) {
+        var gumb = $(this);
+        var ime = gumb.prop("name");
+        $.ajax(
+            {
+                url: window.location.pathname + "?rt=user/otkazanaRezervacija",
+                data: { idVoznje: ime},
+                type: 'POST',
+        		dataType: "json",
+    			success: function( data )
+    			{
+                    window.location.reload(false);
+                    //console.log("uspjesno izbrisana rezervacija");
+    			}
+            }
+        );
+    }
+}
+
+function otkaziVoznju(event) {
+    var ret = confirm("Are you sure you want to delete this drive?");
+    if (ret === true) {
+        var gumb = $(this);
+        var ime = gumb.prop("name");
+        $.ajax(
+            {
+                url: window.location.pathname + "?rt=user/otkazanaVoznja",
+                data: { idVoznje: ime},
+                type: 'POST',
+        		dataType: "json",
+    			success: function( data )
+    			{
+                    window.location.reload(false);
+                    //console.log("uspjesno izbrisana voznja");
+    			}
+            }
+        );
+    }
+}
+
+function procitanaPoruka(event) {
+    alert("The driver canceled this drive!");
+
+    var gumb = $(this);
+    var ime = gumb.prop("name");
+    $.ajax(
+        {
+            url: window.location.pathname + "?rt=user/procitanaPoruka",
+            data: { idVoznje: ime},
+            type: 'POST',
+    		dataType: "json",
+			success: function( data )
+			{
+                window.location.reload(false);
+                console.log("uspjesno procitana poruka");
+			}
+        }
+    );
+
+}
+</script>
 
     <!-- /.container-fluid-->
     <!-- /.content-wrapper-->
     <footer class="sticky-footer">
       <div class="container">
         <div class="text-center">
-          <small>Copyright Â© 2018</small>
+          <small>Copyright © 2018</small>
         </div>
       </div>
     </footer>
