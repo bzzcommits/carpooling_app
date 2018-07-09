36e8b984f39e8e4eb21e551f62cc93648892bda7
matonce <matonce.student.pmf.hr>
2018-07-09 10:43:41 +0200

    Promijenjen znak copyright.

--- view/drive_index.php
+++ view/drive_index.php
@@ -76,7 +76,7 @@ require_once __SITE_PATH . '/view/menuTheRest.php';
     <footer class="sticky-footer">
       <div class="container">
         <div class="text-center">
-          <small>Copyright © 2018</small>
+          <small>Copyright &copy; 2018</small>
         </div>
       </div>
     </footer>
--- view/profile_index.php
+++ view/profile_index.php
@@ -242,7 +242,7 @@ require_once __SITE_PATH . '/view/_header.php';
     <footer class="sticky-footer">
       <div class="container">
         <div class="text-center">
-          <small>Copyright © 2018</small>
+          <small>Copyright &copy; 2018</small>
         </div>
       </div>
     </footer>
